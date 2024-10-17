56324c2db2dae17a18136c8ae07b0ddf53ab3f67
Zack Vera <qphys50@gmail.com>
2024-10-17 15:29:39 -0700

    Added some api fetching functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,13 @@
-function SearchBar() {
-    return <div>SearchBar</div>
+function SearchBar({ onSubmit }) {
+  const handleClick = () => {
+    onSubmit("cars");
+  };
+  return (
+    <div>
+      <input />
+      <button onClick={handleClick}>Click Me</button>
+    </div>
+  );
 }
 
 export default SearchBar;
