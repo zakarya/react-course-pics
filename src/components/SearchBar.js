6aeade70b8e1df93da1cd5ec867bddee6a96966a
Zack Vera <qphys50@gmail.com>
2024-10-17 17:31:35 -0700

    handling form submission in state.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
+
 function SearchBar({ onSubmit }) {
-  const handleClick = () => {
+  const [term, setTerm] = useState("");
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
     onSubmit("cars");
   };
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
+  };
+
   return (
     <div>
-      <input />
-      <button onClick={handleClick}>Click Me</button>
+      <form onSubmit={handleFormSubmit}>
+        <input value={term} onChange={handleChange} />
+      </form>
     </div>
   );
 }
